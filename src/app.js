178cc0a85114ff0e8865086396950fed06aaa0a7
AlirezaDevv <thealireza.am@yahoo.com>
2023-08-08 12:56:54 +0330

    edit

--- src/app.js
+++ src/app.js
@@ -1,132 +1,132 @@
-import startQuiz from "./start-quiz";
 
 
-// const questions = [
-//   {
-//     question: "which is largest animal in the world",
-//     answers: [
-//       { text: "shark", correct: false },
-//       { text: "blue whale", correct: true },
-//       { text: "elephant", correct: false },
-//       { text: "giraffe", correct: false },
-//     ],
-//   },
-//   {
-//     question: "which is the smallest contry in the world",
-//     answers: [
-//       { text: "vatican city", correct: true },
-//       { text: "bhutan", correct: false },
-//       { text: "nepal", correct: false },
-//       { text: "shri lanka", correct: false },
-//     ],
-//   },
-//   {
-//     question: "which is the largest desert in the world",
-//     answers: [
-//       { text: "kalahari", correct: false },
-//       { text: "gobi", correct: false },
-//       { text: "sahara", correct: false },
-//       { text: "antarctica", correct: true },
-//     ],
-//   },
-//   {
-//     question: "which is samllest continent in the world",
-//     answers: [
-//       { text: "asia", correct: false },
-//       { text: "australia", correct: true },
-//       { text: "europe", correct: false },
-//       { text: "africa", correct: false },
-//     ],
-//   },
-// ];
 
-// const question = document.querySelector("#question");
-// const answers = document.querySelector("#answers");
-// const next = document.querySelector("#next");
+const questions = [
+  {
+    question: "which is largest animal in the world",
+    answers: [
+      { text: "shark", correct: false },
+      { text: "blue whale", correct: true },
+      { text: "elephant", correct: false },
+      { text: "giraffe", correct: false },
+    ],
+  },
+  {
+    question: "which is the smallest contry in the world",
+    answers: [
+      { text: "vatican city", correct: true },
+      { text: "bhutan", correct: false },
+      { text: "nepal", correct: false },
+      { text: "shri lanka", correct: false },
+    ],
+  },
+  {
+    question: "which is the largest desert in the world",
+    answers: [
+      { text: "kalahari", correct: false },
+      { text: "gobi", correct: false },
+      { text: "sahara", correct: false },
+      { text: "antarctica", correct: true },
+    ],
+  },
+  {
+    question: "which is samllest continent in the world",
+    answers: [
+      { text: "asia", correct: false },
+      { text: "australia", correct: true },
+      { text: "europe", correct: false },
+      { text: "africa", correct: false },
+    ],
+  },
+];
 
-// let currentQindex = 0;
-// let score = 0;
+const question = document.querySelector("#question");
+const answers = document.querySelector("#answers");
+const next = document.querySelector("#next");
 
-// function startQuiz() {
-//   currentQindex = 0;
-//   score = 0;
-//   next.innerHTML = "Next";
+let currentQindex = 0;
+let score = 0;
 
-//   showQuestion();
-// }
+function startQuiz() {
+  currentQindex = 0;
+  score = 0;
+  next.innerHTML = "Next";
 
-// function showQuestion() {
-//   resetState();
-//   let currentQ = questions[currentQindex];
-//   let questionNo = currentQindex + 1;
-//   question.innerHTML = questionNo + ". " + currentQ.question;
-// let i = 0;
-//   currentQ.answers.forEach((answer) => {
-//     i++;
-//     const button = document.createElement("button");
-//     button.innerHTML = i + ". " + answer.text;
-//     button.classList =
-//       "btn w-full border-2 border-solid border-gray-900 px-5 py-3 rounded-md text-slate-900 text-left my-3 disabled:cursor-no-drop";
-//     answers.appendChild(button);
-//     if (answer.correct) {
-//       button.dataset.correct =  answer.correct;
-//     }
-//     button.addEventListener("click", selectAnswer);
-//   });
-// }
+  showQuestion();
+}
 
-// function resetState() {
-//   next.style.display = "none";
+function showQuestion() {
+  resetState();
+  let currentQ = questions[currentQindex];
+  let questionNo = currentQindex + 1;
+  question.innerHTML = questionNo + ". " + currentQ.question;
+let i = 0;
+  currentQ.answers.forEach((answer) => {
+    i++;
+    const button = document.createElement("button");
+    button.innerHTML = i + ". " + answer.text;
+    button.classList =
+      "btn w-full border-2 border-solid border-gray-900 px-5 py-3 rounded-md text-slate-900 text-left my-3 disabled:cursor-no-drop";
+    answers.appendChild(button);
+    if (answer.correct) {
+      button.dataset.correct =  answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+  });
+}
 
-//   while (answers.firstChild) {
-//     answers.removeChild(answers.firstChild);
-//   }
-// }
+function resetState() {
+  next.style.display = "none";
 
-// function selectAnswer(e) {
-//   const selectedBtn = e.target;
-//   const isCorrect = selectedBtn.dataset.correct === "true";
+  while (answers.firstChild) {
+    answers.removeChild(answers.firstChild);
+  }
+}
 
-//   if (isCorrect) {
-//     selectedBtn.classList.add("correct");
-//     score++;
-//   } else {
-//     selectedBtn.classList.add("incorrect");
-//   }
+function selectAnswer(e) {
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.correct === "true";
 
-//   Array.from(answers.children).forEach((button) => {
-//     if (button.dataset.correct === "true") {
-//       button.classList.add("correct");
-//     }
-//     button.disabled = true;
-//   });
-//   next.style.display = "block";
-// }
+  if (isCorrect) {
+    selectedBtn.classList.add("correct");
+    score++;
+  } else {
+    selectedBtn.classList.add("incorrect");
+  }
 
-// function showScore() {
-//   resetState();
-//   question.innerHTML = `Your Score is ${score} of ${questions.length} `;
+  Array.from(answers.children).forEach((button) => {
+    if (button.dataset.correct === "true") {
+      button.classList.add("correct");
+    }
+    button.disabled = true;
+  });
+  next.style.display = "block";
+}
 
-//   next.innerHTML = "play again";
-//   next.style.display = "block";
-// }
+function showScore() {
+  resetState();
+  question.innerHTML = `Your Score is ${score} of ${questions.length} `;
 
-// function handleNext() {
-//   currentQindex++;
+  next.innerHTML = "start again";
+  next.style.display = "block";
+}
 
-//   if (currentQindex < questions.length) {
-//     showQuestion();
-//   } else {
-//     showScore();
-//   }
-// }
+function handleNext() {
+  currentQindex++;
 
-// next.addEventListener("click", () => {
-//   if (currentQindex < questions.length) {
-//     handleNext(currentQindex);
-//   } else {
-//     startQuiz();
-//   }
-// });
+  if (currentQindex < questions.length) {
+    showQuestion();
+  } else {
+    showScore();
+  }
+}
+
+next.addEventListener("click", () => {
+  if (currentQindex < questions.length) {
+    handleNext(currentQindex);
+  } else {
+    startQuiz();
+  }
+});
 
 startQuiz();
--- src/show-score.js
+++
@@ -1,9 +0,0 @@
-
-
-function showScore() {
-    resetState();
-    question.innerHTML = `Your Score is ${score} of ${questions.length} `;
-  
-    next.innerHTML = "play again";
-    next.style.display = "block";
-  }
